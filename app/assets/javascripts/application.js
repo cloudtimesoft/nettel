9fa62831cf8e1d56033a252adc4206c3fb3b4466
cloudtimesoft <321456532@qq.com>
2015-04-06 17:47:31 +0800

    Css1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,65 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+// require jquery_ujs
+// require turbolinks
+
+
+
+
+
+//= require jquery.min.js
+//= require jquery-ui.js
+//= require bootstrap.min.js
+//= require raphael.min.js
+//= require selectivizr-min.js
+//= require jquery.mousewheel.js
+//= require jquery.vmap.min.js
+//= require jquery.vmap.sampledata.js
+//= require jquery.vmap.world.js
+//= require jquery.bootstrap.wizard.js
+//= require fullcalendar.min.js
+//= require gcal.js
+//= require jquery.dataTables.min.js
+//= require datatable-editable.js
+//= require jquery.easy-pie-chart.js
+//= require excanvas.min.js
+//= require jquery.isotope.min.js
+//= require isotope_extras.js
+//= require modernizr.custom.js
+//= require jquery.fancybox.pack.js
+//= require select2.js
+//= require styleswitcher.js
+//= require wysiwyg.js
+//= require summernote.min.js
+//= require jquery.inputmask.min.js
+//= require jquery.validate.js
+//= require bootstrap-fileupload.js
+//= require bootstrap-datepicker.js
+//= require bootstrap-timepicker.js
+//= require bootstrap-colorpicker.js
+//= require bootstrap-switch.min.js
+//= require typeahead.js
+//= require daterange-picker.js
+//= require date.js
+//= require morris.min.js
+//= require skycons.js
+//= require fitvids.js
+//= require jquery.sparkline.min.js
+//= require main.js
+//= require respond.js
+
+
+
+
+
+
+
+
+
+
+
+
+
 //= require_tree .
+
